3f1104da657d5d2f8ffe6cee88297f9478205fc1
Reganto <tell.reganto@gmail.com>
2022-03-27 22:43:06 +0430

    Ajax POST XMLHttpRequest

---
+++ ajax/ajax-post-xmlhttprequest/server.py
@@ -0,0 +1,29 @@
+import secrets
+
+from tornado.web import Application, url
+from tornado.ioloop import IOLoop
+from usernado import Handler
+
+
+class Home(Handler.Web):
+    def get(self):
+        self.render("home.html")
+
+
+class GhostCall(Handler.Web):
+    def post(self):
+        message = self.get_escaped_argument("message")
+        self.write({"message": message})
+
+
+if __name__ == "__main__":
+    handlers = [
+        url("/", Home, name="home"),
+        url("/call/", GhostCall, name="call"),
+    ]
+    settings = {
+        "debug": True,
+        "cookie_secret": secrets.token_bytes(),
+    }
+    Application(handlers, **settings).listen(8000)
+    IOLoop.current().start()
